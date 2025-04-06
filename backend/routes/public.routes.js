1d1433d417dbd0a501242f7be3192577a9d93b0e
JeevanPavan <jeevanpavan43@gmail.com>
2025-04-06 13:27:40 +0530

    Update public.routes.js

--- backend/routes/public.routes.js
+++ backend/routes/public.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 
 const router = express.Router();
-
+import {downloadController} from '../controllers/publicDataController.js';
 // TODO: Add routes for public data
+router.get('/zip', downloadController.downloadZip);
 
 export default router;
