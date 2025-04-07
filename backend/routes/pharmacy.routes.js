8e9def5b171422047694df9f03b89923276f238c
MishkaUtsav <118451816+MishkaUtsav@users.noreply.github.com>
2025-04-07 00:57:26 +0000

    added pharmacist.controller and modified pharmacy.routes

--- backend/routes/pharmacy.routes.js
+++ backend/routes/pharmacy.routes.js
@@ -3,5 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 // TODO: Add routes for pharmacy
+router.get('/prescriptions', searchPatientPrescriptions);
 
 export default router;
